dfe5c4a4d780d2262105620d1356cc336e2689d4
Eliska Jelinkova <116261184+eljelink@users.noreply.github.com>
2022-11-03 12:09:27 +0100

    details

--- plugins/HelloToYou.hpp
+++ plugins/HelloToYou.hpp
@@ -1,7 +1,7 @@
 /**
  * @file HelloToYou.hpp
  *
- * Developer(s) of this DAQModule have yet to replace this line with a brief description of the DAQModule.
+ * This module receives greeting sentence and responds with a "Hello to you!" answer
  *
  * This is part of the DUNE DAQ Software Suite, copyright 2020.
  * Licensing/copyright details are in the COPYING file that you should have
@@ -14,7 +14,6 @@
 #include "appfwk/DAQModule.hpp"
 #include "iomanager/Receiver.hpp" //because two modules communicate with each other
 #include "utilities/WorkerThread.hpp"
-
 #include "ers/Issue.hpp"
 #include "StringWrapper.hpp"
 
@@ -51,9 +50,9 @@ dunedaq::utilities::WorkerThread thread_;
 void do_work(std::atomic<bool>&);
 
 //Configuration
-using source_t=dunedaq::iomanager::ReceiverConcept<String>; //we will reveive the data from greetingDataQueue
-std::shared_ptr<source_t> greetingDataQueue_; //definition of variable which holds "Hello Name" sentence
-std::chrono::milliseconds queueTimeout_; //definition of queue timeout variable
+using source_t=dunedaq::iomanager::ReceiverConcept<String>;
+std::shared_ptr<source_t> greetingDataQueue_;
+std::chrono::milliseconds queueTimeout_;
 
 // Statistic counters
 std::atomic<int64_t> receivedCount {0};
